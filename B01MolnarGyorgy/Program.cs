75e843ad0df7301ee3686f9c5dc3126fe4fa5355
MlnrGyrgy <molnar.gyorgy.mrgr@gmail.com>
2020-09-14 11:08:01 +0200

    Beolvasas

--- B01MolnarGyorgy/Program.cs
+++ B01MolnarGyorgy/Program.cs
@@ -9,8 +9,21 @@ namespace B01MolnarGyorgy
 {
     class Program
     {
+        static int[] adatok = new int[3000];
+        static void Beolvasas()
+        {
+            StreamReader olvas = new StreamReader("adatok.dat");
+            int i = 0;
+            while (!olvas.EndOfStream)
+            {
+                adatok[i] = int.Parse(olvas.ReadLine());
+                //Console.WriteLine(adatok[i]);
+                i++;
+            }     
+        }
         static void Main(string[] args)
         {
+            Beolvasas();
             Console.ReadKey();
         }
     }
